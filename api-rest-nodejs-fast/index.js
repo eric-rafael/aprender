01b8805b2f14c4404f47c61ea1924d6320b66f7b
eric-rafael <ericrafael753@gmail.com>
2022-04-27 08:26:12 -0300

    add readme

---
+++ api-rest-nodejs-fast/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+// FAKE DATABASE
+let books =[];
+// CRIAR APP
+const app = express();
+
+app.user(express.json());
+
+app.post("/books", (req, res) =>{
+    const { id, title, author, publishedAt } = req.body;
+    const book = { id, title, author, publishedAt };
+    books.push(book);
+    return res.status(201).json(book);
+});
+
+// MANDAR O SERVIDOR RODAR
+
+app.listen(3333, ( => ))
